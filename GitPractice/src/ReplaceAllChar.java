8670363da59bd97f6e5c49bd2af99229c0294bb8
SurajBhor1 <surajbhor22@gmail.com>
2023-07-27 17:02:08 +0530

    'replaceChar'

---
+++ GitPractice/src/ReplaceAllChar.java
@@ -0,0 +1,11 @@
+
+public class ReplaceAllChar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="({suraj})";
+String refined=str.replaceAll("[(){}]", "");
+System.out.println(refined);
+	}
+
+}
